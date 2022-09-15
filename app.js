0e004a66eded76ba6a83f4ad7ec394dd920c5ab3
Anas Baqai <anashabib139@gmail.com>
2022-09-15 09:28:06 -0700

    changing brannch for scd

--- app.js
+++ app.js
@@ -4,39 +4,40 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const _ = require("lodash");
-// const session = require("express-session");
-// const passport = require("passport");
-// const passportLocalMongoose = require("passport-local-mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 app.use(express.static("public"));
 
-// app.use(session({
-//     secret: "we are friends",
-//     resave: false,
-//     saveUninitialized: false,
-// })
-// );
+app.use(session({
+    secret: "we are friends",
+    resave: false,
+    saveUninitialized: false,
+})
+);
 
 
 
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
-mongoose.connect("mongodb+srv://anasbaqai:An12as34@cluster0.uuocn2n.mongodb.net/OgroupStudentsDB");
+// mongoose.connect("mongodb+srv://anasbaqai:An12as34@cluster0.uuocn2n.mongodb.net/OgroupStudentsDB");
+mongoose.connect("mongodb://localhost:27017/sqeDB");
 
-// const usersSchema = new mongoose.Schema({
-//     username: String,
-//     password: String,
-// });
-// usersSchema.plugin(passportLocalMongoose);
-// const User = mongoose.model("User", usersSchema)
+const usersSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+});
+usersSchema.plugin(passportLocalMongoose);
+const User = mongoose.model("User", usersSchema)
 
-// passport.use(User.createStrategy());
-// passport.serializeUser(User.serializeUser());
-// passport.deserializeUser(User.deserializeUser());
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 const testsSchema = new mongoose.Schema({
     date: {
